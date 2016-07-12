e39450093f0e245a9fe5e9511844d72c739a6379
Shogo Tabata <TBT@tbtBook.local>
2016-07-13 00:01:55 +0900

    タッチ入力実装

---
+++ New Unity Project/Assets/Scripts/Core/EventManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class UnityEventIntArg : UnityEvent <int>{}
+public class EventManager : MonoBehaviour {
+	static public UnityEvent OnDestroyEnemy;
+	static public UnityEvent OnDestroyPlayer;
+	static public UnityEvent OnDestroyJoint;
+	static public UnityEvent OnHitSword;
+	// Use this for initialization
+	void Awake() {
+		Debug.Log ("Awake Event");
+		SetEvent (ref OnDestroyEnemy);
+		SetEvent (ref OnDestroyPlayer);
+		SetEvent (ref OnDestroyJoint);
+		SetEvent (ref OnHitSword);
+	}
+	void SetEvent(ref UnityEvent u ){
+		if (u == null) {
+			u = new UnityEvent ();
+		}
+	}
+	void SetEventIntArg(ref UnityEventIntArg u ){
+		if (u == null) {
+			u = new UnityEventIntArg ();
+		}
+	}
+	static public void Invoke(ref UnityEvent u ){
+		if (u != null) {
+			u.Invoke();
+		}
+	}
+	static public void InvokeIntArg(ref UnityEventIntArg u ,int a){
+		if (u != null) {
+			u.Invoke(a);
+		}
+	}
+	// Update is called once per frame
+}
---
+++ New Unity Project/Assets/Scripts/Player/Arm.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arm : MonoBehaviour {
+	private bool isPunching = false;
+	private int punchTime = 0;
+	private int punchTimeMax = 18;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (TouchInput.GetTouch() == TouchInfo.Ended) {
+			if(isPunching == false)StartPunch();
+	}
+		Punching ();
+	}
+	/*private readonly float[] punchPos = new float[]{
+		0f,0.5f,0.85f,0.9f,0.95f,1.0f,0.95f,0.9f,0.85f,0.8f,0.4f
+	};*/
+	float punchPos = 0;
+	void StopPunching(){
+		if (isPunching == true) {
+		}
+	}
+	void Punching(){
+		if (isPunching == true) {
+		punchTime++;
+			punchPos = 1;
+			if(punchTime < 6)punchPos = 1 * punchTime/6.0f;
+			if(punchTimeMax - punchTime < 6)punchPos = (punchTimeMax - punchTime) * 1 / 6.0f;
+		//punchPos = Mathf.Sin(punchTime * (180 / punchTimeMax)  * Mathf.PI/180);
+		//if(punchTime<punchPos.Length)transform.localPosition = new Vector3(-0.05f,punchPos[punchTime],0);
+		transform.localPosition = new Vector3(-0.05f,punchPos,0);
+		if(punchTime>punchTimeMax){
+				transform.localPosition = new Vector3(-0.05f,-0f,0);
+				punchTime = 0;
+				isPunching = false;
+			}
+		}
+
+	}
+	void StartPunch(){
+		isPunching = true;
+	}
+}
